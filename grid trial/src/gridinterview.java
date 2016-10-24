2f35687fc18f5e8a29f0f572044b2be721d2f184
remyavas <remyavas@yahoo.co.in>
2016-10-24 21:32:21 +0530

    first git local commit

---
+++ grid trial/src/gridinterview.java
@@ -0,0 +1,9 @@
+
+public class gridinterview {
+public static void main(String[] args)
+{
+System.out.println("hello");	
+
+}
+	
+}
---
+++ grid trial/src/gridsecond.java
@@ -0,0 +1,10 @@
+
+public class gridsecond {
+	
+	public static void main(String[] args)
+	{
+	System.out.println("hello string");	
+
+	}
+
+}
